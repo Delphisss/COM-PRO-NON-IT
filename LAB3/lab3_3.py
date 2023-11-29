3af6e0d6374f528afcd65e0934c07236883b8cf6
Rose4nne <120131543+Delphisss@users.noreply.github.com>
2023-11-29 14:31:27 +0700

    Update lab3_3.py

--- LAB3/lab3_3.py
+++ LAB3/lab3_3.py
@@ -1,8 +1,6 @@
 product_prices = [float(input(f"Enter product{i + 1}'s price: ")) for i in range(5)]
 vat_rate = float(input("Enter VAT rate (0-100): "))
 budget = float(input("Enter your budget: "))
-
 total_with_vat = sum(price * (1 + vat_rate / 100) for price in product_prices)
 exceeds_budget = total_with_vat > budget
-
 print(f"Total with VAT: {total_with_vat} baht. It exceeds your budget: {exceeds_budget}")
