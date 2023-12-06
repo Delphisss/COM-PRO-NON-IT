9d7fba0e4c0d4396492fe5ca6860930cb1ad6d59
Delphisss <Kong_led_@hotmail.com>
2023-12-06 13:31:07 +0700

    fix lab4

--- LAB4/lab4_3.py
+++ LAB4/lab4_3.py
@@ -12,13 +12,13 @@ for _ in range(lucky_number):
 
 
 
-
+#ส่วนของคนที่สอง คิดคล้ายๆกัน แต่ต้องปรับตรงนามสกุลของคนที่สองที่ต้อง
 new_first_name = input("Please enter a name to replace your first name: ")
 characters_to_strip = input("Please enter characters to be stripped out from your last name: ")
 
-new_full_name = new_first_name.capitalize() + " " + last_name.replace(characters_to_strip, "")
+new_full_name = new_first_name.capitalize() + " " + last_name.replace(characters_to_strip, "") #.replace เพื่อstr ที่ต้องการ มันเป็นเมทธอทของpython
 
-new_lucky_number = int(len(last_name.replace(characters_to_strip, "")) / len(new_first_name))
+new_lucky_number = int(len(last_name.replace(characters_to_strip, "")) / len(new_first_name)) #คิดค่าแบบเดิมคล้ายคนแรกเลย 
 
 print(f"Hi {new_full_name.upper()}, your new lucky number is {new_lucky_number}")
 for _ in range(new_lucky_number):
