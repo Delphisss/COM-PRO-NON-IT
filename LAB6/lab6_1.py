4bb1475812b0e023f75da4958619f1fa82c66758
Rose4nne <120131543+Delphisss@users.noreply.github.com>
2023-12-20 13:28:39 +0700

    Update lab6_1.py

--- LAB6/lab6_1.py
+++ LAB6/lab6_1.py
@@ -1,27 +1,32 @@
-# Example 5: A program that checks if the input numbers meet the criteria.
 print("Program to check if num1 is in [0-9] and num2 is in [10-99].")
 num1 = int(input("Enter num1: "))
 num2 = int(input("Enter num2: "))
-if num1 >= 0 and num1 <= 9:
-    if num2 >= 10 and num2 <= 99:
-print("Both numbers are entered correctly.")
-    else:
-print("Only num1 is entered correctly.")
+
+correct_num1 = 0 <= num1 <= 9
+correct_num2 = 10 <= num2 <= 99
+
+if correct_num1 and correct_num2:
+    print("Both numbers are entered correctly.")
+elif correct_num1:
+    print("Only num1 is entered correctly.")
+elif correct_num2:
+    print("Only num2 is entered correctly.")
 else:
-    if num2 >= 10 and num2 <= 99:
-print("Only num2 is entered correctly.")
-    else:
     print("Both numbers are NOT entered correctly.")
-print("Good bye.")
+print("Goodbye.")
 
+score = int(input("Enter the score: "))
 
-# 1) Re-do Example 5 by using a different approach (not nested if).
-# 2) Write a program that takes an integer score as an input and gives a corresponding letter grade 
-# (see table below). If the score is not within 0-100, print “Error”. You must use if-else-if structure.
-# Score Grade
-# 80-100 A
-# 70-79 B
-# 60-69 C
-# 50-59 D
-# 0-49 F
-# Not in the range 0-100 Erro
+if 0 <= score <= 100:
+    if score >= 80:
+        print("Grade: A")
+    elif score >= 70:
+        print("Grade: B")
+    elif score >= 60:
+        print("Grade: C")
+    elif score >= 50:
+        print("Grade: D")
+    else:
+        print("Grade: F")
+else:
+    print("Error: Score is not within the range 0-100.")
