97b3abcb0478e3cab37b164977d5bb19dfe2af7f
Delphisss <Kong_led_@hotmail.com>
2023-12-23 13:55:27 +0700

    lab6

--- LAB6/lab6_1.py
+++ LAB6/lab6_1.py
@@ -8,12 +8,12 @@ if not enough_items:
 else:
     remaining_items = total_items - ordered_items
 
-    if remaining_items < total_items * 0.5:
-        items_to_fill = total_items - remaining_items
-        print(f"Please fill inventory with {items_to_fill} more items.")
-    else:
-        print("No need to fill inventory at this time.")
+if remaining_items < total_items * 0.5:
+    items_to_fill = total_items - remaining_items
+    print(f"Please fill inventory with {items_to_fill} more items.")
+else:
+    print("No need to fill inventory at this time.")
 
-    print(f"Remaining items in inventory: {remaining_items}")
+print(f"Remaining items in inventory: {remaining_items}")
 
 # print(total_items)
